6820a4619e4d853c472b9c7df2cd5a03b072be09
Seongmin Oh <saogig0503@naver.com>
2025-09-02 22:56:32 +0900

    add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Home from './pages/Home.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
+);
