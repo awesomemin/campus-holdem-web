93acc22c33f92e83dccd1203d7f9cbfce409e59b
Seongmin Oh <saogig0503@naver.com>
2025-09-04 22:56:27 +0900

    modify header design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 function Header() {
   return (
-    <div className="h-12 flex items-center justify-center">성균홀덤대회</div>
+    <div className="h-[60px] flex items-center text-2xl pl-4 shadow-md font-semibold">
+      성균홀덤대회
+    </div>
   );
 }
 
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 function Layout() {
   return (
-    <div className="min-h-screen max-w-md bg-dark-bg mx-auto">
+    <div className="min-h-screen max-w-md mx-auto bg-bg-200">
       <Outlet />
     </div>
   );
