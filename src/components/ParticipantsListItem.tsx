2765b3f06ce77588502f28154b7495960ff6456e
Seongmin Oh <saogig0503@naver.com>
2025-09-05 17:33:09 +0900

    implemented game detail page

---
+++ src/components/ParticipantsListItem.tsx
@@ -0,0 +1,39 @@
+interface Participant {
+  profileImgUrl: string;
+  name: string;
+  ppi: number;
+  isConfirmed: boolean;
+}
+
+interface ParticipantsListItemProps {
+  participant: Participant;
+}
+
+function ParticipantsListItem({ participant }: ParticipantsListItemProps) {
+  if (!participant) {
+    return (
+      <div className="flex items-center justify-center h-11 border-b border-bg-300 text-text-gray">
+        BLANK
+      </div>
+    );
+  }
+  return (
+    <div className="flex items-center h-11 pl-3 border-b border-bg-300">
+      <img
+        className="w-7 h-7 rounded-full mr-2"
+        src={participant.profileImgUrl}
+      />
+      <span className="mr-1">{participant.name}</span>
+      {participant.isConfirmed ? (
+        <span className="text-xs bg-primary-green text-text-black rounded-md px-1 py-0.5">
+          확정
+        </span>
+      ) : (
+        <span className="text-xs bg-bg-300 px-1 py-0.5 rounded-md">대기</span>
+      )}
+      <span className="ml-auto mr-3">{participant.ppi} PPI</span>
+    </div>
+  );
+}
+
+export default ParticipantsListItem;
--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import GameInfoBox from '../components/GameInfoBox';
 import Header from '../components/Header';
+import ParticipantsListItem from '../components/ParticipantsListItem';
 
 interface Game {
   gameId: number;
@@ -14,6 +15,7 @@ interface Game {
   estimatedDurationInMinutes: number;
   maxParticipant: number;
   minimumParticipant: number;
+  participants: Participant[];
 }
 
 interface Participant {
@@ -36,134 +38,152 @@ const fetchGameById = async (id: string): Promise<Game> => {
     estimatedDurationInMinutes: 180,
     maxParticipant: 9,
     minimumParticipant: 6,
+    participants: [
+      {
+        profileImgUrl:
+          'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEggsuge49N6-hA1f27delS42-C0Ku0TeEfXvX0c2-aZgbDiTCCfFnlc9k3p-c0wTqyz87a9lMsSjh6H8pXbTVJQXgLJkbfrg7OaGluIcNyeGLbKn27f2D3mMlzyN-gqenRBhL_XJm9AfAM/s1600-rw/How+Long+Does+it+Take+to+Become+a+Professional+Poker+Player%253F.jpg',
+        name: '홀덤고수',
+        ppi: 1000,
+        isConfirmed: true,
+      },
+      {
+        profileImgUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsps8cftqZt3HKhEVCBl3dTRr5c6vurTOjVg&s',
+        name: '안녕하세요',
+        ppi: 1357,
+        isConfirmed: true,
+      },
+      {
+        profileImgUrl:
+          'https://roarcdn.fitting-solutions.at/mgm/poker/en/blog/wp-content/uploads/2023/10/21115638/Body-Image-2-.jpg?strip=all&lossy=1&ssl=1&fit=1080%2C610',
+        name: '대학원의 망령',
+        ppi: 1000,
+        isConfirmed: true,
+      },
+      {
+        profileImgUrl:
+          'https://roarblogs.s3.amazonaws.com/borgata/casino/en/blog/wp-content/uploads/2023/06/06043337/Header-Tilting-poker-player-ROAR-Bogata-Online.jpg',
+        name: '오예',
+        ppi: 1000,
+        isConfirmed: false,
+      },
+      {
+        profileImgUrl:
+          'https://upswingpoker.com/wp-content/uploads/2019/08/bryn-kenney-1200x675.jpg',
+        name: 'wsop',
+        ppi: 1000,
+        isConfirmed: false,
+      },
+    ],
   };
   return new Promise((resolve) => setTimeout(() => resolve(gameData), 500));
-};
-
-const fetchParticipantsById = async (id: string): Promise<Participant[]> => {
-  console.log(id);
-  const participants = [
-    { profileImgUrl: '123', name: '홀덤고수', ppi: 1000, isConfirmed: true },
-    { profileImgUrl: '456', name: '안녕하세요', ppi: 1357, isConfirmed: true },
-    {
-      profileImgUrl: '789',
-      name: '대학원의 망령',
-      ppi: 1000,
-      isConfirmed: true,
-    },
-    { profileImgUrl: '111', name: '오예', ppi: 1000, isConfirmed: false },
-    { profileImgUrl: '222', name: 'wsop', ppi: 1000, isConfirmed: false },
-  ];
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(participants), 1000);
-  });
 };
 
 function GameDetail() {
   const { gameId } = useParams<{ gameId: string }>();
   const [gameInfo, setGameInfo] = useState<Game | null>(null);
-  const [gameInfoLoading, setGameInfoLoading] = useState<boolean>(true);
-  const [participants, setParticipants] = useState<Participant[] | null>(null);
-  const [participantsLoading, setParticipantsLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadGameData = async () => {
       if (!gameId) {
-        setGameInfoLoading(false);
+        setLoading(false);
         return;
       }
-      setGameInfoLoading(true);
+      setLoading(true);
       const data = await fetchGameById(gameId);
       setGameInfo(data);
-      setGameInfoLoading(false);
-    };
-    const loadParticipantsData = async () => {
-      if (!gameId) {
-        setParticipantsLoading(false);
-        return;
-      }
-      setParticipantsLoading(true);
-      const data = await fetchParticipantsById(gameId);
-      setParticipants(data);
-      setParticipantsLoading(false);
+      setLoading(false);
     };
     loadGameData();
-    loadParticipantsData();
   }, [gameId]);
 
   if (!gameId) return <div>not found</div>;
+  if (loading || !gameInfo) return <div>loading...</div>;
+
+  const emptySlotsCount =
+    gameInfo.maxParticipant - gameInfo.participants.length;
+  const emptySlots = Array(Math.max(0, emptySlotsCount)).fill(null);
+  const displayList = [...gameInfo.participants, ...emptySlots];
 
   return (
     <>
       <Header />
-      {!gameInfo ? (
-        <div>loading...</div>
-      ) : (
-        <GameInfoBox
-          gameId={gameId}
-          time={gameInfo.time}
-          place={gameInfo.place}
-        />
-      )}
+      <GameInfoBox
+        gameId={gameId}
+        time={gameInfo.time}
+        place={gameInfo.place}
+      />
 
       {/* 기본정보 */}
-      {gameInfoLoading || !gameInfo ? (
-        <div>loading...</div>
-      ) : (
-        <div className="py-5 pl-5 border-b-8 border-bg-300">
-          <div className="font-medium text-xl">기본정보</div>
-          <div className="grid grid-cols-[72px_1fr] gap-x-5 gap-y-2 mt-3 ml-1">
-            <div className="flex justify-between font-semibold">
-              <span>참</span>
-              <span>가</span>
-              <span>비</span>
-            </div>
-            <span>{gameInfo.particifationFee}원</span>
-
-            <div className="flex justify-between font-semibold">
-              <span>리</span>
-              <span>바</span>
-              <span>인</span>
-              <span>비</span>
-            </div>
-            <span>{gameInfo.rebuyinFee}원</span>
-
-            <div className="flex justify-between font-semibold">
-              <span>스</span>
-              <span>타</span>
-              <span>팅</span>
-              <span>칩</span>
-            </div>
-            <span>
-              {gameInfo.startingChip} ({gameInfo.startingBB}BB)
-            </span>
-
-            <div className="flex justify-between font-semibold">
-              <span>예</span>
-              <span>상</span>
-              <span>시</span>
-              <span>간</span>
-            </div>
-            <span>{gameInfo.estimatedDurationInMinutes}분</span>
+      <div className="py-5 pl-5 border-b-8 border-bg-300">
+        <div className="font-medium text-xl">기본정보</div>
+        <div className="grid grid-cols-[72px_1fr] gap-x-5 gap-y-2 mt-3 ml-1">
+          <div className="flex justify-between font-semibold">
+            <span>참</span>
+            <span>가</span>
+            <span>비</span>
           </div>
+          <span>{gameInfo.particifationFee}원</span>
+
+          <div className="flex justify-between font-semibold">
+            <span>리</span>
+            <span>바</span>
+            <span>인</span>
+            <span>비</span>
+          </div>
+          <span>{gameInfo.rebuyinFee}원</span>
+
+          <div className="flex justify-between font-semibold">
+            <span>스</span>
+            <span>타</span>
+            <span>팅</span>
+            <span>칩</span>
+          </div>
+          <span>
+            {gameInfo.startingChip} ({gameInfo.startingBB}BB)
+          </span>
+
+          <div className="flex justify-between font-semibold">
+            <span>예</span>
+            <span>상</span>
+            <span>시</span>
+            <span>간</span>
+          </div>
+          <span>{gameInfo.estimatedDurationInMinutes}분</span>
         </div>
-      )}
+      </div>
 
       {/* 기본정보 끝 */}
       {/* 참가자 */}
-      {participantsLoading || !participants || !gameInfo ? (
-        <div>loading...</div>
-      ) : (
-        <div className="pl-5 pt-5">
-          <div>
-            <span className="font-medium text-xl mr-1">참가자</span>
-            <span className="font-light">
-              ({participants.length}/{gameInfo.maxParticipant})
-            </span>
-          </div>
+      <div className="pl-5 pt-5 pb-20">
+        <div>
+          <span className="font-medium text-xl mr-1">참가자</span>
+          <span className="font-light">
+            ({gameInfo.participants.length}/{gameInfo.maxParticipant})
+          </span>
         </div>
-      )}
+        <div className="flex flex-col mr-5 mt-3">
+          {displayList.map((participant) => (
+            <ParticipantsListItem participant={participant} />
+          ))}
+        </div>
+      </div>
       {/* 참가자 끝 */}
+      {/* 하단바 */}
+      <div className="fixed bg-bg-300 bottom-0 w-full max-w-md flex px-5 items-center justify-between h-16 shadow-[0_-4px_4px_-1px_rgba(0,0,0,0.1)]">
+        <div className="text-xs font-light">
+          {gameInfo.minimumParticipant < gameInfo.participants.length ? (
+            <span>경기 진행이 확정되었어요</span>
+          ) : (
+            <span>경기 확정까지 1명 남았어요</span>
+          )}
+        </div>
+        <button className="h-9 w-32 rounded-sm bg-text-white text-text-black">
+          신청하기
+        </button>
+      </div>
+      {/* 하단바 끝 */}
     </>
   );
 }
