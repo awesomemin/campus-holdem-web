9a35bfe8bbd65a4778aa1b9fb239901cb8a2fe8f
Seongmin Oh <saogig0503@naver.com>
2025-09-06 07:47:12 +0900

    connect header account button to login page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ function Header() {
   return (
     <div className="h-[60px] flex items-center text-2xl pl-4 shadow-md font-semibold">
       <Link to="/">성균홀덤대회</Link>
-      <Person className="ml-auto mr-[18px]" />
+      <Link className="ml-auto mr-[18px]" to="/login">
+        <Person />
+      </Link>
     </div>
   );
 }
