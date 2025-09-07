9531a174e4ac54ee7715658e81e841e50b032928
Seongmin Oh <saogig0503@naver.com>
2025-09-07 10:52:42 +0900

    fix gameinfo box ui crash

--- src/components/GameInfoBox.tsx
+++ src/components/GameInfoBox.tsx
@@ -6,7 +6,7 @@ interface GameInfoBoxProps {
 
 function GameInfoBox({ gameId, time, place }: GameInfoBoxProps) {
   return (
-    <div className="h-[123] flex flex-col pt-6 pb-5 pl-5 border-b-8 border-bg-300">
+    <div className="flex flex-col pt-6 pb-5 pl-5 border-b-8 border-bg-300">
       <div className="text-xl font-light">#{gameId}</div>
       <div className="mt-2 font-semibold text-xl">{time}</div>
       <div className="mt-1">{place}</div>
