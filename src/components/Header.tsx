78fc0046dda565637cda2b2fcf692e2ab0b1b899
Seongmin Oh <saogig0503@naver.com>
2025-09-15 22:23:17 +0900

    header my-button direct connects to my-page when logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router';
 import Person from '@mui/icons-material/Person';
+import { useAuth } from '../contexts/AuthContext';
 
 function Header() {
+  const { user } = useAuth();
   return (
     <div className="h-[60px] flex items-center text-2xl pl-4 shadow-md font-semibold">
       <Link to="/">성균홀덤대회</Link>
-      <Link className="ml-auto mr-[18px]" to="/login">
+      <Link
+        className="ml-auto mr-[18px]"
+        to={user ? `/user/${user.userId}` : 'login'}
+      >
         <Person />
       </Link>
     </div>
